154c9122fad91a09c10480ef28c4be78a89c789b
rayhanferdous <mferdous152079@bba.uiu.ac.bd>
2025-01-12 11:03:24 +0600

    update init style on all pages

--- index.php
+++ index.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Apex Test Repo</title>
+    <link rel="stylesheet" href="./assets/style.css">
 </head>
+
 <body>
-    
+    <header>
+        <nav>
+            <ul>
+                <li style="background-color: gray;"><a href="./index.php">Home</a></li>
+                <li><a href="./pages/products.php">Products</a></li>
+                <li><a href="./pages/cart.php">Cart</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="page-header">
+            <h2>Apex Test Repo - Products Page</h2>
+            <a href="./pages/product/create_product.php">Create Product</a>
+        </div>
+        <table class="page-table">
+            <tr>
+                <th>Id</th>
+                <th>Product Name</th>
+                <th>Product category</th>
+                <th>Dynamic Options</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>John Doe</td>
+                <td>johndoe@gmail.com</td>
+                <td>johndoe@gmail.com</td>
+            </tr>
+        </table>
+    </main>
 </body>
+
 </html>
---
+++ pages/products.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apex Test Repo</title>
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="../index.php">Home</a></li>
+                <li style="background-color: gray;"><a href="../pages/products.php">Products</a></li>
+                <li><a href="../pages/cart.php">Cart</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="page-header">
+            <h2>Apex Test Repo - Product Page</h2>
+            <a href="../index.php">Products Page</a>
+        </div>
+        <!-- Products create page content goes here -->
+    </main>
+</body>
+
+</html>
